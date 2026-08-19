3981ad52ded8477df97dc1ce735adfd238114367
Melchior <64213755+MelchMwoan@users.noreply.github.com>
2026-08-19 16:41:15 +0200

    Added support for auto restart HA on integration update

--- hapro/rootfs/usr/src/request.sh
+++ hapro/rootfs/usr/src/request.sh
@@ -4,13 +4,16 @@ doSupervisorRequest() {
     local url=$1
     local method=${2:-"GET"}
     local data=${3:-""}
-    response=$(curl -s -X "${method}" -H "X-Supervisor-Token: $(printenv SUPERVISOR_TOKEN)" -d "${data}" "http://supervisor/${url}")
+    local clean_url="${url#/}"
+    local bearer=$(printenv SUPERVISOR_TOKEN)
+    response=$(curl -s -X "${method}" -H "X-Supervisor-Token: $bearer" -H "Authorization: Bearer $bearer" -d "${data}" "http://supervisor/${clean_url}")
 }
 
 doHaInternalApiRequest() {
     local url=$1
     local method=${2:-"GET"}
     local data=${3:-""}
+    local clean_url="${url#/}"
     local bearer=$(printenv SUPERVISOR_TOKEN)
-    response=$(curl -s -X "${method}" -H "Authorization: Bearer $bearer" -d "${data}" "http://supervisor/core/api/${url}")
+    response=$(curl -s -X "${method}" -H "Authorization: Bearer $bearer" -d "${data}" "http://supervisor/core/api/${clean_url}")
 }
